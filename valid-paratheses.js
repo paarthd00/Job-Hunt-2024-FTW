b910d688e86b85450e6dafd0a4981a1c00094f96
paarthd00 <paarthcode@gmail.com>
2024-02-28 03:17:47 -0800

    valid parantheses stack solution

---
+++ valid-paratheses.js
@@ -0,0 +1,23 @@
+var isValid = function(s) {
+    let stack = [];
+    let map = {
+        "(": ")",
+        "{": "}",
+        "[": "]"
+    };
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "(" || s[i] === "{" || s[i] === "[") {
+            stack.push(s[i]);
+        } else {
+            let last = stack.pop();
+            if (s[i] !== map[last]) {
+                return false;
+            }
+        }
+    }
+    if (stack.length !== 0) {
+        return false;
+    }
+    return true;
+    
+};
