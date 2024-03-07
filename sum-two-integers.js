71fbcba8d54922959a20ead04075d1ed02f425da
paarthd00 <paarthcode@gmail.com>
2024-03-07 13:06:08 -0800

    add explanation for sum two integers

--- sum-two-integers.js
+++ sum-two-integers.js
@@ -1,8 +1,31 @@
+/**
+Given two integers a and b, return the sum of the two integers without using the operators + and -.
+
+ 
+
+Example 1:
+
+Input: a = 1, b = 2
+Output: 3
+Example 2:
+
+Input: a = 2, b = 3
+Output: 5
+ 
+
+Constraints:
+
+-1000 <= a, b <= 1000 
+
+
+
+ */
+
 function getSum(a, b) {
-    while (b != 0) {
-      const carry = (a & b) << 1; // Calculate carry (note: handles negative numbers in JS)
-      a = a ^ b;            // Calculate sum without carry
-      b = carry;            // Update carry 
-    }
-    return a;
+  while (b != 0) {
+    const carry = (a & b) << 1; // Calculate carry (note: handles negative numbers in JS)
+    a = a ^ b; // Calculate sum without carry
+    b = carry; // Update carry
   }
+  return a;
+}
